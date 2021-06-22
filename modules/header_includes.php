891b371026dbd3bf384fa67d8f176ad8d10820fd
ih par <ismail@cs.deu.edu.tr>
2021-06-22 16:52:44 +0300

    header newline fix

--- modules/header_includes.php
+++ modules/header_includes.php
@@ -4,7 +4,6 @@ if(!$is_secure) {
 }
 $css_ver = "?v=3";
 ?>
-
 <meta charset="UTF-8" />
 <meta http-equiv="X-UA-Compatible" content="IE=edge" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
