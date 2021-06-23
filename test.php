46f87fee9de6169714a3bba725c066b8443699b0
ihpar <ismail@cs.deu.edu.tr>
2021-06-23 22:33:44 +0300

    testing

--- test.php
+++ test.php
@@ -3,8 +3,7 @@ require_once "dbms/class.db.php";
 require_once "dbms/utils.php";
 $db = DB::getInstance();
 
-$user_types = $db->get_results( "SELECT * FROM user_types" );
-foreach( $user_types as $user ) {
-var_dump($user) . '<br />';
+$user_types = $db->get_results("SELECT * FROM user_types");
+foreach ($user_types as $user) {
+    var_dump($user) . '<br />';
 }
-?>
