ca8c5da6450214018976b07b31a72bc377427195
ih par <ismail@cs.deu.edu.tr>
2021-06-24 14:01:25 +0300

    dil

--- langs/lang_index.php
+++ langs/lang_index.php
@@ -10,7 +10,7 @@ $lang_index = [
         "tr" => "Amaç ve Kapsam"
     ],
     "amac_ve_kapsam_metin" => [
-        "en" => "<div>
+        "en" => '<div>
       <p>
       Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce at urna at felis molestie feugiat. Sed vitae blandit nulla, nec porttitor diam. Donec dapibus finibus eros vitae maximus. Nunc ornare lectus sit amet nulla aliquam aliquet. Sed facilisis magna et gravida imperdiet. Cras a sapien tristique, tempus enim et, interdum felis. Vestibulum porta libero nec ligula cursus lobortis. Integer et purus et eros convallis sollicitudin.
       </p>
@@ -18,7 +18,7 @@ $lang_index = [
       Phasellus in placerat dolor. Nullam eget libero mattis mi porta faucibus sit amet quis sapien. Fusce fermentum, libero ut luctus malesuada, sapien mauris efficitur nibh, vel bibendum enim neque vel eros. Phasellus viverra, turpis in hendrerit eleifend, sapien risus porta nulla, vel imperdiet ipsum ipsum sit amet arcu. Vestibulum eu magna in neque interdum vulputate sit amet nec mauris. Integer interdum augue in dolor eleifend, id mattis quam convallis. Curabitur porttitor a eros nec rutrum. Vivamus vel enim risus. Integer volutpat turpis ut tellus efficitur, in ornare dui interdum.
       </p>
     </div>
-    <div id=\"div-truncated\" class=\"trunc\">
+    <div id="div-truncated" class="trunc">
       <p>
       Mauris pharetra, velit suscipit volutpat sodales, massa lorem luctus augue, non porta arcu augue id justo. Praesent facilisis consectetur metus vel ultrices. Nullam felis eros, imperdiet ut tortor a, feugiat hendrerit urna. In eget metus ut justo tempor accumsan id eu dolor. Vivamus id felis fringilla, molestie nibh in, dignissim sapien. Nam iaculis, ex in placerat consectetur, risus mi iaculis nisl, a pellentesque massa orci id odio. In hac habitasse platea dictumst. Vestibulum vel nisi eget est rutrum sodales. In aliquet ante vitae libero pharetra porttitor. Donec odio velit, bibendum ut tellus ac, egestas ornare neque.
       </p>
@@ -29,8 +29,8 @@ $lang_index = [
       Donec imperdiet vehicula tortor, eu tincidunt libero consectetur in. Curabitur lectus enim, lacinia vel lobortis vel, imperdiet vel ante. Nulla arcu massa, aliquet et nisl id, sagittis eleifend nisl. Nulla facilisi. Proin vitae suscipit orci. Aliquam sem mauris, eleifend eu neque imperdiet, pharetra interdum erat. Pellentesque tincidunt fringilla varius. Cras volutpat neque at nulla pretium, nec mollis lacus sagittis. Sed blandit, urna eu elementum auctor, diam dui vehicula augue, eget lacinia mi diam eget quam. Ut pellentesque nisi id nisl congue gravida. Proin consequat erat pretium arcu vestibulum facilisis. Nulla finibus augue at convallis ullamcorper.
       </p>
     </div>
-    <a href=\"#\" id=\"show-p-trunc\" data-state=\"collapsed\">Continue...</a>",
-        "tr" => "<div>
+    <a href="#" id="show-p-trunc" data-state="collapsed">Continue...</a>',
+        "tr" => '<div>
       <p>
         Yönelim, içinde pek çok zamanı, değeri ve felsefeyi barındıran çok özellikli bir kavram, kavramlar
         üstü bir gerçekliktir. İnsan yaşamında kaçınılmaz olarak ortaya çıkan yönelim aslında hayatın her
@@ -48,7 +48,7 @@ $lang_index = [
         bir çözüm arayışı içinde “daha olana hareketi” olarak açıklanabilir. Tıpkı müzikte olduğu gibi…
       </p>
     </div>
-    <div id=\"div-truncated\" class=\"trunc\">
+    <div id="div-truncated" class="trunc">
       <p>
         Hiçbir zaman durağan olmayan müzik tarihi, öncesine tepkiyle yeniye yönelen müzik, akım ve
         dönemlerle doludur ve yönelimlerin hiçbirisi kısa süreler içinde gerçekleşmemiştir. Barok
@@ -74,7 +74,7 @@ $lang_index = [
         etrafında şekillenen değerli çalışmalarınızı bekliyoruz.
       </p>
     </div>
-    <a href=\"#\" id=\"show-p-trunc\" data-state=\"collapsed\">Devamı...</a>"
+    <a href="#" id="show-p-trunc" data-state="collapsed">Devamı...</a>'
     ],
     "devami" => [
         "en" => "Continue...",
