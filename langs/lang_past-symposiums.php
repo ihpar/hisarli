d1f2024d42d7129fb98c86708f2663a647faed9d
ihpar <ismail@cs.deu.edu.tr>
2021-07-08 23:27:48 +0300

    inner static pages

--- langs/lang_past-symposiums.php
+++ langs/lang_past-symposiums.php
@@ -1,25 +1,1 @@
-<?php
-
-
-if (!$is_secure) {
-
-    exit(0);
-
-}
-
-
-$lang_past_symposiums = [
-
-    "gecmis_sempozyumlar" => [
-        "en" => "Past Symposiums",
-        "tr" => "Geçmiş Sempozyumlar"
-    ],
-    "ilan_edilecektir" => [
-        "en" => "Coming soon.",
-        "tr" => "Yapım aşamasındadır."
-    ]
-
-];
-
-
-?>
+<?phpif (!$is_secure) {    exit(0);}$lang_past_symposiums = [    "gecmis_sempozyumlar_title" => [        "en" => "Past Symposiums",        "tr" => "Geçmiş Sempozyumlar"    ],    "download_proceeding" => [        "en" => "Click to Download the Proceedings Book.",        "tr" => "Bildiri Kitapçığını İndirmek için Tıklayınız."    ],    "2009" => [        "en" => "            <p>Participants: 10</p>            <p>Concerts: 1</p>                                ",        "tr" => "            <p>Katılımcı: 10</p>            <p>Konser: 1</p>        "    ],    "2011" => [        "en" => "            <p>Participants: 62</p>            <p>Concerts: 8</p>            <p>Panels: 1</p>                                ",        "tr" => "            <p>Katılımcı: 62</p>            <p>Konser: 8</p>            <p>Panel: 1</p>        "    ],    "2012" => [        "en" => "            <p>Participants: 85</p>            <p>Concerts: 10</p>            <p>Conferences: 1</p>            <p>Exhibitions: 1</p>            <p>Panels: 1</p>                ",        "tr" => "            <p>Katılımcı: 85</p>            <p>Konser: 10</p>            <p>Konferans: 1</p>            <p>Sergi: 1</p>            <p>Panel: 1</p>        "    ],    "2013" => [        "en" => "            <p>Participants: 85</p>            <p>Concerts: 14</p>            <p>Conferences: 1</p>            <p>Exhibitions: 1</p>            <p>Panels: 1</p>                ",        "tr" => "            <p>Katılımcı: 85</p>            <p>Konser: 14</p>            <p>Konferans: 1</p>            <p>Sergi: 1</p>            <p>Panel: 1</p>        "    ],    "2014" => [        "en" => "            <p>Participants: 51</p>            <p>Concerts: Soma Mine Disaster / Canceled</p>            <p>Conferences: 1</p>            <p>Exhibitions: 2</p>            <p>Panels: 1</p>                ",        "tr" => "            <p>Katılımcı: 51</p>            <p>Konser: Soma Maden Faciası / İptal</p>            <p>Konferans: 1</p>            <p>Sergi: 2</p>            <p>Panel: 1</p>        "    ],    "2015" => [        "en" => "            <p>Participants: 55</p>            <p>Concerts: 12</p>            <p>Conferences: 1</p>            <p>Exhibitions: 1</p>            <p>Panels: 1</p>                ",        "tr" => "            <p>Katılımcı: 55</p>            <p>Konser: 12</p>            <p>Konferans: 1</p>            <p>Sergi: 1</p>            <p>Panel: 1</p>        "    ],    "2016" => [        "en" => "            <p>Participants: 52</p>            <p>Concerts: 14</p>            <p>Conferences: 6</p>            <p>Exhibitions: 2</p>            <p>Panels: 1</p>                ",        "tr" => "            <p>Katılımcı: 52</p>            <p>Konser: 14</p>            <p>Konferans: 6</p>            <p>Sergi: 2</p>            <p>Panel: 1</p>        "    ],    "2017" => [        "en" => "            <p>Participants: 52</p>            <p>Concerts: 12</p>            <p>Conferences: 2</p>            <p>Exhibitions: 2</p>            <p>Panels: 1</p>            <p>Workshops: 2</p>                ",        "tr" => "            <p>Katılımcı: 52</p>            <p>Konser: 12</p>            <p>Konferans: 2</p>            <p>Sergi: 2</p>            <p>Panel: 1</p>            <p>Atölye Çalışması: 2</p>        "    ],    "2018" => [        "en" => "            <p>Participants: 55</p>            <p>Concerts: 14</p>            <p>Conferences: 2</p>            <p>Exhibitions: 2</p>            <p>Panels: 1</p>            <p>Workshops: 2</p>                ",        "tr" => "            <p>Katılımcı: 55</p>            <p>Konser: 14</p>            <p>Konferans: 2</p>            <p>Sergi: 2</p>            <p>Panel: 1</p>            <p>Atölye Çalışması: 2</p>        "    ],    "2019" => [        "en" => "            <p>Participants: 73</p>            <p>Concerts: 18</p>            <p>Conferences: 2</p>            <p>Exhibitions: 2</p>            <p>Panels: 1</p>            <p>Workshops: 2</p>                ",        "tr" => "            <p>Katılımcı: 73</p>            <p>Konser: 18</p>            <p>Konferans: 2</p>            <p>Sergi: 2</p>            <p>Panel: 1</p>            <p>Atölye Çalışması: 2</p>        "    ],    "2021" => [        "en" => "            <p>Participants: 111</p>            <p>Concerts: 20</p>            <p>Conferences: 6</p>            <p>Exhibitions: 3</p>                ",        "tr" => "            <p>Katılımcı: 111</p>            <p>Konser: 20</p>            <p>Konferans: 6</p>            <p>Sergi: 3</p>        "    ]];?>
