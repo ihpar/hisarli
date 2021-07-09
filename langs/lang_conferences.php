a42550c0657e021eee99dc4e477c1d4ee325980f
ihpar <ismail@cs.deu.edu.tr>
2021-07-09 18:05:13 +0300

    inner static pages

---
+++ langs/lang_conferences.php
@@ -0,0 +1,20 @@
+<?php
+
+if (!$is_secure) {
+    exit(0);
+}
+
+
+$lang_conferences = [
+
+    "konferans" => [
+        "en" => "Conferences",
+        "tr" => "Konferanslar"
+    ],
+    "ilan_edilecektir" => [
+        "en" => "To be announced later.",
+        "tr" => "Program daha sonra ilan edilecektir."
+    ]
+];
+
+?>
---
+++ langs/lang_workshops.php
@@ -0,0 +1,20 @@
+<?php
+
+if (!$is_secure) {
+    exit(0);
+}
+
+
+$lang_workshops = [
+
+    "atolye_calismalari" => [
+        "en" => "Workshops",
+        "tr" => "Atölye Çalışmaları"
+    ],
+    "ilan_edilecektir" => [
+        "en" => "To be announced later.",
+        "tr" => "Program daha sonra ilan edilecektir."
+    ]
+];
+
+?>
