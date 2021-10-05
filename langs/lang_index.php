88dcf44130d6b1e0c412b62280a824d521ba752c
ih par <ismail@cs.deu.edu.tr>
2021-10-05 10:47:57 +0300

    new abstract submission form

--- langs/lang_index.php
+++ langs/lang_index.php
@@ -11,80 +11,78 @@ $lang_index = [
     ],
     "amac_ve_kapsam_metin" => [
         "en" => '<div>
-<p>The affections of soundscapes created by Antonio Vivaldi (The Four Seasons),
-the impressions imagined by Claude Debussy (Footsteps in the Snow), the mimesis of the ornithological
-sound records by Olivier Messiaen (Oiseaux Exotiques), the Altai-Sayan ecoregion expressed by Sainkho Namtchylak
-(Stepmother City) show that climate is present in the spectrum of styles and music history. Geographical characteristics,
-ecosystems and themes related to sets of specific atmospheric conditions that exert influence on the life are
-references observed in musical inspiration, that trigger thoughts and emotions in the listener. </p>
-
-<p>However, this source of environmentalist incitement drastically change in the twenty-first
-century. Caused by overexploration and greenhouse gases-based economy, the planet Earth faces
-a climate emergency that is accelerating faster than scientists expected*, and whose situation
-may be irreversible for the next generations of the human societies**. Climate change entered
-the list of major global problems becoming subject and clause of world politic meetings
-(Intergovernmental Panel on Climate Change, Kyoto Protocol, Paris Agreement, World
-Economic Forum). Efforts to mitigate and adapt for the impacts of the climate change should not
-be seen as a mere environmental problem, as it will be implemented with radical changes in the
-lifestyle and behavioural of humanity.</p>
-<!--</div>-->
-
-<!--<div id="div-truncated" class="trunc">-->
-<p>Following the tradition of focusing on human and society established for over a decade
-(Acculturation in a Changing Society, Music Perception, Where is Music Heading for?, Music
-and City, Sound-Timbre-Perception, Music and Therapy, Media and Technology, Music Theory,
-Music in Human Life, New Tendencies in Music) the Hisarlı Ahmet Symposium determines
-<span style="font-weight: bold">Music and Climate</span> as the edition of the year <span style="font-weight: bold"> 2022 </span>.</p>
-   
-<p>Aims to face one of the greatest global challenges, seeking to understand the role of
-musicians in the face of climate problems and how music can help in this intergenerational
-change, the 12th Hisarlı Ahmet International Symposium are honored to invite all academitians to
-contribute in a multidisciplinary approach reinforcing critical communication within and across
-disciplines.</p>
-   
-<p>*Ripple, W. J., et al (2021). World Scientists’ Warning of a Climate Emergency 2021, BioScience, Vol. 70/1, 8-12</p>
-<p>**Steffenet, W., et al (2018). Trajectories of Earth System in the Anthropocene, Procedings of the National Academy of Sciences 115, 8252-8259</p>
-</div>
-
-<!--<a href="#" id="show-p-trunc" data-state="collapsed">Continue...</a>-->',
+                    <p>The affections of soundscapes created by Antonio Vivaldi (The Four Seasons),
+                    the impressions imagined by Claude Debussy (Footsteps in the Snow), the mimesis of the ornithological
+                    sound records by Olivier Messiaen (Oiseaux Exotiques), the Altai-Sayan ecoregion expressed by Sainkho Namtchylak
+                    (Stepmother City) show that climate is present in the spectrum of styles and music history. Geographical characteristics,
+                    ecosystems and themes related to sets of specific atmospheric conditions that exert influence on the life are
+                    references observed in musical inspiration, that trigger thoughts and emotions in the listener. </p>
+                    
+                    <p>However, this source of environmentalist incitement drastically change in the twenty-first
+                    century. Caused by overexploration and greenhouse gases-based economy, the planet Earth faces
+                    a climate emergency that is accelerating faster than scientists expected*, and whose situation
+                    may be irreversible for the next generations of the human societies**. Climate change entered
+                    the list of major global problems becoming subject and clause of world politic meetings
+                    (Intergovernmental Panel on Climate Change, Kyoto Protocol, Paris Agreement, World
+                    Economic Forum). Efforts to mitigate and adapt for the impacts of the climate change should not
+                    be seen as a mere environmental problem, as it will be implemented with radical changes in the
+                    lifestyle and behavioural of humanity.</p>
+                    <!--</div>-->
+                    
+                    <!--<div id="div-truncated" class="trunc">-->
+                    <p>Following the tradition of focusing on human and society established for over a decade
+                    (Acculturation in a Changing Society, Music Perception, Where is Music Heading for?, Music
+                    and City, Sound-Timbre-Perception, Music and Therapy, Media and Technology, Music Theory,
+                    Music in Human Life, New Tendencies in Music) the Hisarlı Ahmet Symposium determines
+                    <span style="font-weight: bold">Music and Climate</span> as the edition of the year <span style="font-weight: bold"> 2022 </span>.</p>
+                       
+                    <p>Aims to face one of the greatest global challenges, seeking to understand the role of
+                    musicians in the face of climate problems and how music can help in this intergenerational
+                    change, the 12th Hisarlı Ahmet International Symposium are honored to invite all academitians to
+                    contribute in a multidisciplinary approach reinforcing critical communication within and across
+                    disciplines.</p>
+                       
+                    <p>*Ripple, W. J., et al (2021). World Scientists’ Warning of a Climate Emergency 2021, BioScience, Vol. 70/1, 8-12</p>
+                    <p>**Steffenet, W., et al (2018). Trajectories of Earth System in the Anthropocene, Procedings of the National Academy of Sciences 115, 8252-8259</p>
+                </div>
+                <!--<a href="#" id="show-p-trunc" data-state="collapsed">Continue...</a>-->',
         "tr" => '<div>
-      <p>Antonio Vivaldi\'nin “Dört Mevsim” ile yarattığı ses manzaralarının duygulanımları,
-Claude Debussy\'nin “Karda Ayak İzleri”ndeki empresyonist imajları, Olivier Messiaen\'in
-ornitolojik “Egzotik Kuşlar” ses kayıtları mimetiği ve Sainkho Namtchylak\'ın “Üvey Şehir”inde
-ses bulan Altay-Sayan ekolojik bölgesi, iklimin müzik tarihi ve müzik stilleri spektrumunun bir
-parçası olduğunu gösteriyor. Coğrafi özellikler, ekosistemler ve yaşamı etkileyen belirli
-atmosferik koşullar kümeleriyle ilgili temalar, müzikal ilhamda gözlemlenen, dinleyicide
-düşünce ve duyguları tetikleyen referanslardır.</p>
-
-<p>Ancak bu ilham verici kaynak XXI. yüzyılda büyük ölçüde değişime uğramıştır. Aşırı
-tüketim ve sera gazlarına dayalı ekonomi, Dünya gezegenini bilim insanlarının beklediğinden
-çok daha hızlı bir süreç içinde* gelecek nesil insan toplumları için geri dönülemez bir acil iklim
-durumuyla yüz yüze getirecektir**. İklim değişikliği, uluslararası politik platformların
-(Hükümetlerarası İklim Değişikliği Paneli, Kyoto Protokolü, Paris İklim Anlaşması, Dünya
-Ekonomik Forumu) ana konusu ve maddesi haline gelerek başlıca küresel sorunlar listesine girdi.
-İklim değişikliğinin etkilerini azaltma ve bunlara uyum sağlama çabaları, insanlığın yaşam tarzı
-ve davranışlarında köklü değişikliklere sebep olacağı için salt bir çevre sorunu olarak
-görülmemelidir.</p>
-<!--</div>-->
-
-<!--<div id="div-truncated" class="trunc">-->
-<p>On yılı aşkın bir süredir; Değişen Toplumda Kültürlenme ve Kültürleşme, Müzik Algısı,
-Müzik Nereye Gidiyor?, Şehir ve Müzik, Müzik Dinleme ve Çalgı Performansında Ses-Tını-
-Algı, Müzik ve Terapi, Müzik Medya ve Teknoloji, Müzik Teorileri, İnsan Yaşamında Müzik ve
-Müzikte Yeni Yönelimler temalarıyla yerleşik olan insana ve topluma odaklanma geleneğinin
-ardından, Hisarlı Ahmet Sempozyumu <span style="font-weight: bold"> 2022 </span> yılı için
-<span style="font-weight: bold"> Müzik ve İklim </span> konusunu belirledi.</p>
-   
-<p>Büyük küresel zorluklardan biriyle yüzleşmeyi amaçlayan 12. Uluslararası Hisarlı Ahmet
-Sempozyumu tüm akademisyenleri disiplinlerarası ve disiplinlerüstü eleştirel iletişimi
-güçlendirici bir yaklaşımla iklim sorunları karşısında müzisyenlerin rolü ve müziğin nesillerarası
-değişime yardım olanakları konularına katkıda bulunmaya davet etmekten onur duyar.</p>
-   
-<p>*Ripple, W. J., et al (2021). World Scientists’ Warning of a Climate Emergency 2021, BioScience, Vol. 70/1, 8-12</p>
-<p>**Steffenet, W., et al (2018). Trajectories of Earth System in the Anthropocene, Procedings of the National Academy of Sciences 115, 8252-8259</p>
-</div>
-
-<!--<a href="#" id="show-p-trunc" data-state="collapsed">Continue...</a>-->'
+                    <p>Antonio Vivaldi\'nin “Dört Mevsim” ile yarattığı ses manzaralarının duygulanımları,
+                    Claude Debussy\'nin “Karda Ayak İzleri”ndeki empresyonist imajları, Olivier Messiaen\'in
+                    ornitolojik “Egzotik Kuşlar” ses kayıtları mimetiği ve Sainkho Namtchylak\'ın “Üvey Şehir”inde
+                    ses bulan Altay-Sayan ekolojik bölgesi, iklimin müzik tarihi ve müzik stilleri spektrumunun bir
+                    parçası olduğunu gösteriyor. Coğrafi özellikler, ekosistemler ve yaşamı etkileyen belirli
+                    atmosferik koşullar kümeleriyle ilgili temalar, müzikal ilhamda gözlemlenen, dinleyicide
+                    düşünce ve duyguları tetikleyen referanslardır.</p>
+                    
+                    <p>Ancak bu ilham verici kaynak XXI. yüzyılda büyük ölçüde değişime uğramıştır. Aşırı
+                    tüketim ve sera gazlarına dayalı ekonomi, Dünya gezegenini bilim insanlarının beklediğinden
+                    çok daha hızlı bir süreç içinde* gelecek nesil insan toplumları için geri dönülemez bir acil iklim
+                    durumuyla yüz yüze getirecektir**. İklim değişikliği, uluslararası politik platformların
+                    (Hükümetlerarası İklim Değişikliği Paneli, Kyoto Protokolü, Paris İklim Anlaşması, Dünya
+                    Ekonomik Forumu) ana konusu ve maddesi haline gelerek başlıca küresel sorunlar listesine girdi.
+                    İklim değişikliğinin etkilerini azaltma ve bunlara uyum sağlama çabaları, insanlığın yaşam tarzı
+                    ve davranışlarında köklü değişikliklere sebep olacağı için salt bir çevre sorunu olarak
+                    görülmemelidir.</p>
+                    <!--</div>-->
+                    
+                    <!--<div id="div-truncated" class="trunc">-->
+                    <p>On yılı aşkın bir süredir; Değişen Toplumda Kültürlenme ve Kültürleşme, Müzik Algısı,
+                    Müzik Nereye Gidiyor?, Şehir ve Müzik, Müzik Dinleme ve Çalgı Performansında Ses-Tını-
+                    Algı, Müzik ve Terapi, Müzik Medya ve Teknoloji, Müzik Teorileri, İnsan Yaşamında Müzik ve
+                    Müzikte Yeni Yönelimler temalarıyla yerleşik olan insana ve topluma odaklanma geleneğinin
+                    ardından, Hisarlı Ahmet Sempozyumu <span style="font-weight: bold"> 2022 </span> yılı için
+                    <span style="font-weight: bold"> Müzik ve İklim </span> konusunu belirledi.</p>
+                       
+                    <p>Büyük küresel zorluklardan biriyle yüzleşmeyi amaçlayan 12. Uluslararası Hisarlı Ahmet
+                    Sempozyumu tüm akademisyenleri disiplinlerarası ve disiplinlerüstü eleştirel iletişimi
+                    güçlendirici bir yaklaşımla iklim sorunları karşısında müzisyenlerin rolü ve müziğin nesillerarası
+                    değişime yardım olanakları konularına katkıda bulunmaya davet etmekten onur duyar.</p>
+                       
+                    <p>*Ripple, W. J., et al (2021). World Scientists’ Warning of a Climate Emergency 2021, BioScience, Vol. 70/1, 8-12</p>
+                    <p>**Steffenet, W., et al (2018). Trajectories of Earth System in the Anthropocene, Procedings of the National Academy of Sciences 115, 8252-8259</p>
+                </div>
+                <!--<a href="#" id="show-p-trunc" data-state="collapsed">Continue...</a>-->'
     ],
     "devami" => [
         "en" => "Continue...",
@@ -190,13 +188,13 @@ değişime yardım olanakları konularına katkıda bulunmaya davet etmekten onu
     ],
     "duyurular_liste" => [
         "en" => [
+            "The abstract submission module has been activated on October 1st. In order to participate the symposium with your paper, you can fill out the form completely and send it by clicking <a href=\"abstract-submission.php\">Abstract Submission</a> button on the main menu.",
             "The first online working meeting of the 12th International Hisarlı Ahmet Symposium was held on September 10, 2021. The next meeting is on December 10.",
-            "The invitation of 12th International Hisarlı Ahmet Symposium has been announced above. The starting date and sending method of abstract submission will be announced later.",
             "For all your questions and comments: <a href=\"mailto:contact@hisarliahmet.org\">contact@hisarliahmet.org</a>"
         ],
         "tr" => [
+            "Özet modülümüz 1 Ekim itibariyle aktif edilmiştir. Sempozyuma bildirinizle katılmak için ana menüdeki <a href=\"abstract-submission.php\">Özet Gönderim</a> butonuna tıklayarak formu eksiksiz doldurup gönderebilirsiniz.",
             "12. Uluslararası Hisarlı Ahmet Sempozyumu çevrimiçi ilk çalışma toplantısı 10 Eylül 2021'de yapıldı. Bir sonraki toplantı Aralık 10'da. <a href=\"announcements/firststudymeeting.pdf\">Detaylar için tıklayınız...</a>",
-            "12. Uluslararası Hisarlı Ahmet Sempozyum çağrı metni ve web sayfası ilan edilmiştir. ÖZET GÖNDERİM BAŞLANGIÇ TARİHİ ve YÖNTEMİ DAHA SONRA BURADAN DUYURULACAKTIR.",
             "Tüm soru ve görüşlerinizi <a href=\"mailto:contact@hisarliahmet.org\">contact@hisarliahmet.org</a> adresine iletebilirsiniz.</a>"
         ]
     ],
